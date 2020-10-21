0f7bcc1e1b2bd14ea359120f7c64d25def78d030
hongki09 <73209580+hongki09@users.noreply.github.com>
2020-10-21 10:37:34 +0700

    Add files via upload
    first commit

---
+++ IF10K-10119915-Latihan13-Relational/src/if10k/pkg10119915/latihan13/relational/IF10K10119915Latihan13Relational.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119915.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama      : M Hongki Alfikram
+ * NIM       : 10119915
+ * Kelas     : IF-10K
+ * Deskripsi : Relational.
+ */
+public class IF10K10119915Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int bill = 10;
+        int bill2 = 20;
+        
+        System.out.println("a == b = " + (bill == bill2));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("a >= b = " + (bill2 >= bill));
+        System.out.println("a <= b = " + (bill2 <= bill));
+        
+    }
+    
+}
